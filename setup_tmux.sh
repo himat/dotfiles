4e3b30742f6eb0382ce4fef35e48de4afd8cbe92
Hima T <himat415@gmail.com>
2022-11-14 14:28:07 -0500

    Add script to install tmux plugins

---
+++ setup_tmux.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo "Enter tmux and then Press prefix + I (capital i, as in Install) to install all the plugins."
+echo "If you're already in a tmux session, then run `tmux source ~/.tmux.conf` first to apply any changes"
